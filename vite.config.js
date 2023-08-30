aa33e5c6bcf4dc5b0dd881bd7fff3bffc6f060cf
Ivetta Dashkova <ivettafsd@gmail.com>
2023-08-30 23:28:39 +0200

    delete import svgn

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react()],
   resolve: {
     alias: {
       src: "/src",
